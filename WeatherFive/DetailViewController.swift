17ebb531a828ff3defa2cb1d3e0fb2aa86574e2c
Nikolas Omelianov <nikolas@Nikolass-Mac-mini.local>
2017-11-18 16:32:29 +0200

    replace outlets to outlet collection

--- WeatherFive/DetailViewController.swift
+++ WeatherFive/DetailViewController.swift
@@ -25,22 +25,9 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var weekWeatherLabel: UILabel!
     
     @IBOutlet weak var changeAmount: UIButton!
-    @IBOutlet weak var day1: UITextView!
-    @IBOutlet weak var day2: UITextView!
-    @IBOutlet weak var day3: UITextView!
-    @IBOutlet weak var day4: UITextView!
-    @IBOutlet weak var day5: UITextView!
-    @IBOutlet weak var day6: UITextView!
-    @IBOutlet weak var day7: UITextView!
-    
-    @IBOutlet weak var temperature1: UITextView!
-    @IBOutlet weak var temperature2: UITextView!
-    @IBOutlet weak var temperature3: UITextView!
-    @IBOutlet weak var temperature4: UITextView!
-    @IBOutlet weak var temperature5: UITextView!
-    @IBOutlet weak var temperature6: UITextView!
-    @IBOutlet weak var temperature7: UITextView!
-    
+
+    @IBOutlet var collection:[UITextView]!
+    @IBOutlet var collectionofDateTextViews:[UITextView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,31 +35,18 @@ class DetailViewController: UIViewController {
         cityInfoLabel.text = cityInfodata
         
         changeAmount.setTitle("Show more", for: .normal)
-        day4.isHidden = true
-        day5.isHidden = true
-        day6.isHidden = true
-        day7.isHidden = true
-        temperature4.isHidden = true
-        temperature5.isHidden = true
-        temperature6.isHidden = true
-        temperature7.isHidden = true
+        for item in 3..<7{
+            collection[item].isHidden = true
+            collectionofDateTextViews[item].isHidden = true
+        }
+
       
            if  arrayOfWeekDays.count > 0 {
-            day1.text = arrayOfDate[0] + separator + arrayOfWeekDays[0]
-            day2.text = arrayOfDate[1] + separator + arrayOfWeekDays[1]
-            day3.text = arrayOfDate[2] + separator + arrayOfWeekDays[2]
-            day4.text = arrayOfDate[3] + separator + arrayOfWeekDays[3]
-            day5.text = arrayOfDate[4] + separator + arrayOfWeekDays[4]
-            day6.text = arrayOfDate[5] + separator + arrayOfWeekDays[5]
-            day7.text = arrayOfDate[6] + separator + arrayOfWeekDays[6]
-       
-            temperature1.text = morningTemperature[0] + celsius + separator + nightTemperature[0] + celsius
-            temperature2.text = morningTemperature[1] + celsius + separator + nightTemperature[1] + celsius
-            temperature3.text = morningTemperature[2] + celsius + separator + nightTemperature[2] + celsius
-            temperature4.text = morningTemperature[3] + celsius + separator + nightTemperature[3] + celsius
-            temperature5.text = morningTemperature[4] + celsius + separator + nightTemperature[4] + celsius
-            temperature6.text = morningTemperature[5] + celsius + separator + nightTemperature[5] + celsius
-            temperature7.text = morningTemperature[6] + celsius + separator + nightTemperature[6] + celsius
+            for item in 0..<7{
+            collection[item].text = arrayOfDate[item] + separator + arrayOfWeekDays[item]
+           collectionofDateTextViews[item].text = morningTemperature[item] + celsius + separator + nightTemperature[item] + celsius
+            }
+
         }
     }
     
@@ -123,29 +97,22 @@ class DetailViewController: UIViewController {
    
     func changeDayAmount() {
         
-        if day4.isHidden == true {
+        if collection[4].isHidden == true {
             changeAmount.setTitle("Show less", for: .normal)
-            
-            day4.isHidden = false
-            day5.isHidden = false
-            day6.isHidden = false
-            day7.isHidden = false
-            temperature4.isHidden = false
-            temperature5.isHidden = false
-            temperature6.isHidden = false
-            temperature7.isHidden = false
+            for item in 3..<7{
+                collection[item].isHidden = false
+                collectionofDateTextViews[item].isHidden = false
+            }
+
         }
         else{
             changeAmount.setTitle("Show more", for: .normal)
-            
-            day4.isHidden = true
-            day5.isHidden = true
-            day6.isHidden = true
-            day7.isHidden = true
-            temperature4.isHidden = true
-            temperature5.isHidden = true
-            temperature6.isHidden = true
-            temperature7.isHidden = true
+           
+            for item in 3..<7{
+                collection[item].isHidden = true
+                collectionofDateTextViews[item].isHidden = true
+            }
+
         }
     }
     //LAST ROW IN CONTROLLER
